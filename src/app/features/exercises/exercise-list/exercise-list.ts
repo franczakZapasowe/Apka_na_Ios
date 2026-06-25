6f642e423705da78394c323cab2bf48bf2c030e2
MFrancza14 <mirekfranczak14@gmail.com>
2026-06-26 01:54:13 +0200

    feat: dodanie mozliwosci tworzenia i usuwania cwiczen, aktualizacja readme

--- src/app/features/exercises/exercise-list/exercise-list.ts
+++ src/app/features/exercises/exercise-list/exercise-list.ts
@@ -21,4 +21,34 @@ export class ExerciseListComponent implements OnInit{
     // 2. Wrzucamy pobrane dane do naszego Sygnału
     this.exercises.set(data);
   }
+
+  async addExercise(newName :string, category:string){
+    // Zabezpieczenie: sprawdzamy, czy nazwa nie jest pusta
+    // (używamy .trim(), żeby odrzucić same spacje)
+    if (!newName || newName.trim() === '') {
+      return;
+    }
+    //dodajemy nowy do bazy 
+    await db.excercises.add({name:newName, category:category});
+    // aktualizujemy baze 
+    const newData = await db.excercises.toArray();
+
+    this.exercises.set(newData);
+  }
+
+  async removeExcercise(id:number|undefined){
+
+    if (!id){
+      return;
+    }
+    
+    try{
+        await db.excercises.delete(id)
+    }catch(error){
+      console.error("Bład podczas usuwania",error)
+    }
+
+    const newData2 = await db.excercises.toArray();
+    this.exercises.set(newData2);
+  }
 }
