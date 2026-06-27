10530d1fc6dff9f03a89c43643296766133fc075
MFrancza14 <mirekfranczak14@gmail.com>
2026-06-27 15:20:38 +0200

    feat: dodanie systemu zakladek, kalkulacji 1RM, usuwania pojedynczych serii i historii treningow

--- src/app/features/workout/workout-logger/workout-logger.ts
+++ src/app/features/workout/workout-logger/workout-logger.ts
@@ -1,12 +1,12 @@
 import { Component,OnInit,inject, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { db } from '../../../core/db/index-db.service';
 import { WorkoutSet } from '../../../core/models/workout-model';
 import { WorkoutLog } from '../../../core/models/workout-model';
 
 @Component({
   selector: 'app-workout-logger',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './workout-logger.html',
   styleUrl: './workout-logger.css',
 })
@@ -15,7 +15,13 @@ export class WorkoutLogger implements OnInit {
   private route = inject(ActivatedRoute);
   exerciseId:string|null =null;
 
+  // Pojedynczy workoku 
   todayLog = signal<WorkoutLog | undefined>(undefined);
+  // sygnal czy trening dzisaj czy historia treningow
+  activeTab = signal<String>('today');
+  //lista historycznych treningow
+  historyLog = signal<WorkoutLog[]>([]);
+
 
   async ngOnInit() {
     // Nasłuchujemy na zmiany w adresie URL
@@ -23,9 +29,11 @@ export class WorkoutLogger implements OnInit {
         //Pobieramy dokładnie ten parametr, który zdefiniowaliśmy w app.routes.ts ('id')
       this.exerciseId = params.get('id');
       this.zaladujDzisiejszyTrening();
+      this.zaladujHistorie();
     })
   }
 
+  //wysietlanie seri
   async zaladujDzisiejszyTrening(){
     const exid = Number(this.exerciseId);
     const dzisiejszaData = new Date().toISOString().split('T')[0];
@@ -34,7 +42,8 @@ export class WorkoutLogger implements OnInit {
   }
 
   
-   async zapiszSerie(ciezar:HTMLInputElement, powtorzenia:HTMLInputElement){
+  //metoda do zapisu seri 
+   async zapiszSerie(ciezar:HTMLInputElement, powtorzenia:HTMLInputElement, isFailure:boolean){
     const waga = Number(ciezar.value);
     const powt = Number(powtorzenia.value)
     const exid = Number(this.exerciseId);
@@ -42,17 +51,21 @@ export class WorkoutLogger implements OnInit {
     const newSet: WorkoutSet={
       weight:waga,
       reps : powt,
-      isFailure:false
+      isFailure:isFailure
     }
     
     const dzisiejszaData = new Date().toISOString().split('T')[0]
 
     const exsistingLog =await db.workoutLog.where({excerciseId: exid, date:dzisiejszaData}).first();
 
-
+    const oneRepMax:number =Math.round((waga * (1+powt/30))); 
+    
     if (exsistingLog){
       exsistingLog.sets.push(newSet);
       exsistingLog.totalVolume += waga*powt;
+      if(oneRepMax> exsistingLog.estimatedOneRepMax){
+        exsistingLog.estimatedOneRepMax = oneRepMax        
+      }
       // Używamy PUT, żeby podmienić starą wersję rekordu na tę nową, zaktualizowaną
       await db.workoutLog.put(exsistingLog)
 
@@ -62,16 +75,51 @@ export class WorkoutLogger implements OnInit {
         date: dzisiejszaData,
         sets: [newSet],
         totalVolume: waga*powt,
-        estimatedOneRepMax: 0
+        estimatedOneRepMax: oneRepMax
       }
       await db.workoutLog.add(nowyWorkout);
     }
-
     
     ciezar.value ='';
     powtorzenia.value ='';
-
+    
     await this.zaladujDzisiejszyTrening();
+    await this.zaladujHistorie();
 
   }
+
+  //metoda do usuwania pojedynczej seri 
+  async removeSerie(index:number){
+    //pobieramy obecny stan całego treningu z naszego Sygnału
+    const currentLog = this.todayLog();
+
+    if(!currentLog){
+      return;
+    }
+
+    // REKALKULACJA TONAŻU:
+    // Zanim wyrzucimy serię z tablicy, musimy sprawdzić, ile ważyła, żeby odjąć to od totalVolume
+    const setToRemove = currentLog.sets[index];
+    const volumeToSubtract = setToRemove.weight * setToRemove.reps;
+
+    currentLog.totalVolume -= volumeToSubtract;
+
+    currentLog.sets.splice(index,1);
+
+    await db.workoutLog.put(currentLog);
+    await this.zaladujDzisiejszyTrening()
+
+  }
+
+  // metoda do pobierania calej listy treningow
+  async zaladujHistorie(){
+    const exid = Number(this.exerciseId);
+
+    const historia = await db.workoutLog
+      .filter(log => log.excerciseId === exid)
+      .toArray();
+    this.historyLog.set(historia);
+
+  } 
+  
 }
